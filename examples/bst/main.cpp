016e14a61df2cd621bc360bf0b0624f4e4e8a28a
Victor Wildner <vwildner@redhat.com>
2022-11-02 14:11:05 -0300

    fix: update header for bst

--- examples/bst/main.cpp
+++ examples/bst/main.cpp
@@ -1,4 +1,4 @@
-#include "BinaryTree.hpp"
+#include "BST.hpp"
 
 void printNode( TreeNode *node )
 {
@@ -13,14 +13,14 @@ int main()
 {
     BST *tree = new BST();
 
+    tree->insert( 5 );
     tree->insert( 2 );
     tree->insert( 1 );
     tree->insert( 3 );
     tree->insert( 7 );
-    tree->insert( 10 );
     tree->insert( 2 );
-    tree->insert( 5 );
     tree->insert( 8 );
+    tree->insert( 10 );
     tree->insert( 6 );
     tree->insert( 4 );
 
