45d96646b428e73e69f37fb302d98de9ca7b23b8
Victor Wildner <vwildner@redhat.com>
2022-11-06 23:39:05 -0300

    fix: update iterator traits

--- includes/iterator_traits.hpp
+++ includes/iterator_traits.hpp
@@ -5,6 +5,12 @@
 
 namespace ft {
 
+class input_iterator_tag {};
+class output_iterator_tag {};
+class forward_iterator_tag : public input_iterator_tag {};
+class bidirectional_iterator_tag : public forward_iterator_tag {};
+class random_access_iterator_tag : public bidirectional_iterator_tag {};
+
 template < typename Category,
            typename T,
            typename Distance  = ptrdiff_t,
@@ -32,21 +38,43 @@ struct iterator_traits
 template < typename T >
 struct iterator_traits<T *>
 {
-    typedef T                               value_type;
-    typedef ptrdiff_t                       difference_type;
-    typedef T                              *pointer;
-    typedef T                              &reference;
-    typedef std::random_access_iterator_tag iterator_category;
+    typedef T                          value_type;
+    typedef ptrdiff_t                  difference_type;
+    typedef T                         *pointer;
+    typedef T                         &reference;
+    typedef random_access_iterator_tag iterator_category;
 };
 
 template < typename T >
 struct iterator_traits<const T *>
 {
-    typedef T                               value_type;
-    typedef ptrdiff_t                       difference_type;
-    typedef const T                        *pointer;
-    typedef const T                        &reference;
-    typedef std::random_access_iterator_tag iterator_category;
+    typedef T                          value_type;
+    typedef ptrdiff_t                  difference_type;
+    typedef const T                   *pointer;
+    typedef const T                   &reference;
+    typedef random_access_iterator_tag iterator_category;
+};
+
+// Iterator operations
+template <class InputIt>
+typename iterator_traits<InputIt>::difference_type distance( InputIt first,
+                                                             InputIt last )
+{
+    typename iterator_traits<InputIt>::difference_type result = 0;
+    while ( first != last ) {
+        ++first;
+        ++result;
+    }
+    return result;
+};
+
+template <class InputIt, class Distance>
+void advance( InputIt &it, Distance n )
+{
+    while ( n > 0 ) {
+        --n;
+        ++it;
+    }
 };
 
 } // namespace ft
