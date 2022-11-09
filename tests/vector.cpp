02d53c8d7c284d9eb5b2d220f248f3970efc1e73
Victor Wildner <vwildner@redhat.com>
2022-11-09 01:42:03 -0300

    feat: add 90%+ test coverage for vector

--- tests/vector.cpp
+++ tests/vector.cpp
@@ -20,6 +20,20 @@ void test_setup()
 
 void test_teardown() {}
 
+MU_TEST( test_vec_integral_constructor )
+{
+    ft::vector<int> vec( TEST_VECTOR_SIZE, 42 );
+    for ( int i = 0; i < ( int ) vec.size(); i++ )
+        mu_assert( vec[i] == 42, "all vector values must be 42" );
+}
+
+MU_TEST( test_vec_iterators_constructor )
+{
+    ft::vector<int> vec( vecInt.begin(), vecInt.end() );
+    for ( int i = 0; i < ( int ) vec.size(); i++ )
+        mu_assert( vec[i] == vecInt[i], "all vector values must be equal" );
+}
+
 MU_TEST( test_vec_begin )
 {
     mu_assert( *( vecInt.begin() ) == 0,
@@ -83,20 +97,20 @@ MU_TEST( test_vec_max_size )
 
 MU_TEST( test_vec_reserve )
 {
-    ft::vector<int> vecInt2( TEST_VECTOR_SIZE );
-    vecInt2.reserve( 2 * TEST_VECTOR_SIZE );
+    ft::vector<int> vecMut( TEST_VECTOR_SIZE );
+    vecMut.reserve( 2 * TEST_VECTOR_SIZE );
 
-    mu_assert( vecInt2.capacity() >= 2 * TEST_VECTOR_SIZE,
+    mu_assert( vecMut.capacity() >= 2 * TEST_VECTOR_SIZE,
                "vector capacity is greater than or equal to the number of "
                "elements reserved" );
 }
 
 MU_TEST( test_vec_resize )
 {
-    ft::vector<int> vecInt2( TEST_VECTOR_SIZE );
+    ft::vector<int> vecMut( TEST_VECTOR_SIZE );
 
-    vecInt2.resize( 5 );
-    mu_assert( vecInt2.size() == 5, "vector size is equal to the new size" );
+    vecMut.resize( 5 );
+    mu_assert( vecMut.size() == 5, "vector size is equal to the new size" );
 }
 
 MU_TEST( test_vec_capacity )
@@ -254,63 +268,157 @@ MU_TEST( test_vec_const_data )
 
 MU_TEST( test_vec_assign )
 {
-    ft::vector<int> vecInt2( TEST_VECTOR_SIZE );
-    vecInt2.assign( 10, 42 );
-    mu_assert_int_eq( 42, vecInt2[0] );
+    ft::vector<int> vecMut( TEST_VECTOR_SIZE );
+    vecMut.assign( 10, 42 );
+    mu_assert_int_eq( 42, vecMut[0] );
 }
 
 MU_TEST( test_vec_push_back )
 {
-    ft::vector<int> vecInt2( TEST_VECTOR_SIZE );
-    vecInt2.push_back( 42 );
-    mu_assert_int_eq( 42, vecInt2[TEST_VECTOR_SIZE] );
+    ft::vector<int> vecMut( TEST_VECTOR_SIZE );
+    vecMut.push_back( 42 );
+    mu_assert_int_eq( 42, vecMut[TEST_VECTOR_SIZE] );
 }
 
 MU_TEST( test_vec_pop_back )
 {
-    ft::vector<int> vecInt2( TEST_VECTOR_SIZE );
-    vecInt2.push_back( 42 );
-    vecInt2.push_back( 43 );
-    vecInt2.pop_back();
-    mu_assert_int_eq( *( --vecInt2.end() ), 42 );
+    ft::vector<int> vecMut( TEST_VECTOR_SIZE );
+    vecMut.push_back( 42 );
+    vecMut.push_back( 43 );
+    vecMut.pop_back();
+    mu_assert_int_eq( *( --vecMut.end() ), 42 );
 }
 
 MU_TEST( test_vec_insert_pos_val )
 {
-    ft::vector<int> vecInt2( TEST_VECTOR_SIZE );
-    vecInt2.insert( vecInt2.begin(), 42 );
-    mu_assert_int_eq( 42, vecInt2[0] );
+    ft::vector<int> vecMut( TEST_VECTOR_SIZE );
+    vecMut.insert( vecMut.begin(), 42 );
+    mu_assert_int_eq( 42, vecMut[0] );
 }
 
 MU_TEST( test_vec_insert_pos_nsize_val )
 {
-    ft::vector<int>                   vecInt2( TEST_VECTOR_SIZE );
-    ft::random_access_iterator<int *> position   = vecInt2.begin();
+    ft::vector<int>                   vecMut( TEST_VECTOR_SIZE );
+    ft::random_access_iterator<int *> position   = vecMut.begin();
     std::allocator<int>::size_type    n          = 10;
     std::allocator<int>::value_type   value_type = 42;
 
-    vecInt2.insert( position, n, value_type );
+    vecMut.insert( position, n, value_type );
     for ( int i = 0; i < ( int ) n; i++ )
-        mu_assert_int_eq( 42, vecInt2[i] );
+        mu_assert_int_eq( 42, vecMut[i] );
 }
 
 MU_TEST( test_vec_insert_pos_range )
 {
-    ft::vector<int> vecInt2( TEST_VECTOR_SIZE );
-    vecInt2.insert( vecInt2.begin(), vecInt.begin(), vecInt.end() );
-    mu_assert_int_eq( 0, vecInt2[0] );
+    ft::vector<int> vecMut( TEST_VECTOR_SIZE );
+    vecMut.insert( vecMut.begin(), vecInt.begin(), vecInt.end() );
+    mu_assert_int_eq( 0, vecMut[0] );
 }
 
-MU_TEST( test_vec_erase_pos )
+MU_TEST( test_vec_erase_first )
 {
-    ft::vector<int> vecInt2( TEST_VECTOR_SIZE );
-    vecInt2 = vecInt;
-    vecInt2.erase( vecInt2.begin() );
-    mu_assert_int_eq( 1, vecInt2[0] );
+    ft::vector<int> vecMut( TEST_VECTOR_SIZE );
+    vecMut = vecInt;
+    vecMut.erase( vecMut.begin() );
+    for ( int i = 0; i < ( int ) vecMut.size(); i++ )
+        mu_assert( i + 1 == vecMut[i], "values are shifted forward" );
+    mu_assert( vecMut.size() == ( TEST_VECTOR_SIZE - 1 ),
+               "size is reduced by 1" );
+}
+
+MU_TEST( test_vec_erase_range )
+{
+    ft::vector<int> vecMut( TEST_VECTOR_SIZE );
+    vecMut = vecInt;
+    vecMut.erase( vecMut.begin(), vecMut.begin() + 5 );
+    for ( int i = 0; i < ( int ) vecMut.size(); i++ )
+        mu_assert( i + 5 == vecMut[i], "values are shifted forward" );
+    mu_assert( vecMut.size() == ( TEST_VECTOR_SIZE - 5 ),
+               "size is reduced by 5" );
+}
+
+MU_TEST( test_vec_swap_empty )
+{
+    ft::vector<int> vecMut;
+    ft::vector<int> vecMut2;
+    vecMut.insert( vecMut.begin(), 42 );
+    vecMut.swap( vecMut2 );
+    mu_assert_int_eq( 42, vecMut2[0] );
+    mu_assert_int_eq( 0, vecMut.size() );
+    vecMut.swap( vecMut2 );
+    mu_assert_int_eq( 42, vecMut[0] );
+    mu_assert_int_eq( 0, vecMut2.size() );
+}
+
+MU_TEST( test_vec_swap )
+{
+    ft::vector<int> vecMut( TEST_VECTOR_SIZE, 42 );
+    ft::vector<int> vecMut2( TEST_VECTOR_SIZE, 43 );
+    vecMut.swap( vecMut2 );
+    mu_assert_int_eq( 43, vecMut[0] );
+    mu_assert_int_eq( 42, vecMut2[0] );
+}
+
+MU_TEST( test_vec_clear )
+{
+    ft::vector<int> vecMut( TEST_VECTOR_SIZE, 42 );
+    vecMut.clear();
+    mu_assert_int_eq( 0, vecMut.size() );
+}
+
+MU_TEST( test_vec_allocator )
+{
+    ft::vector<int> vecMut( vecInt );
+    mu_assert( vecMut.get_allocator() == vecInt.get_allocator(),
+               "allocator is the same" );
+}
+
+MU_TEST( test_vec_lesser_than )
+{
+    ft::vector<int> vecMut( TEST_VECTOR_SIZE, 42 );
+    ft::vector<int> vecMut2( TEST_VECTOR_SIZE, 43 );
+    mu_assert( vecMut < vecMut2, "vecMut is lesser than vecMut2" );
+}
+
+MU_TEST( test_vec_less_than_or_equal )
+{
+    ft::vector<int> vecMut( TEST_VECTOR_SIZE, 42 );
+    ft::vector<int> vecMut2( TEST_VECTOR_SIZE, 43 );
+    mu_assert( vecMut <= vecMut2, "vecMut is lesser than vecMut2" );
+}
+
+MU_TEST( test_vec_greater_than )
+{
+    ft::vector<int> vecMut( TEST_VECTOR_SIZE, 42 );
+    ft::vector<int> vecMut2( TEST_VECTOR_SIZE, 43 );
+    mu_assert( vecMut2 > vecMut, "vecMut2 is greater than vecMut" );
+}
+
+MU_TEST( test_vec_greater_than_or_equal )
+{
+    ft::vector<int> vecMut( TEST_VECTOR_SIZE, 42 );
+    ft::vector<int> vecMut2( TEST_VECTOR_SIZE, 43 );
+    mu_assert( vecMut2 >= vecMut, "vecMut2 is greater than vecMut" );
+}
+
+MU_TEST( test_vec_equal )
+{
+    ft::vector<int> vecMut( TEST_VECTOR_SIZE, 42 );
+    ft::vector<int> vecMut2( TEST_VECTOR_SIZE, 42 );
+    mu_assert( vecMut == vecMut2, "vecMut is equal to vecMut2" );
+}
+
+MU_TEST( test_vec_not_equal )
+{
+    ft::vector<int> vecMut( TEST_VECTOR_SIZE, 42 );
+    ft::vector<int> vecMut2( TEST_VECTOR_SIZE, 43 );
+    mu_assert( vecMut != vecMut2, "vecMut is not equal to vecMut2" );
 }
 
 MU_TEST_SUITE( suite_vector )
 {
+    MU_RUN_TEST( test_vec_integral_constructor );
+    MU_RUN_TEST( test_vec_iterators_constructor );
     MU_RUN_TEST( test_vec_begin );
     MU_RUN_TEST( test_vec_end );
     MU_RUN_TEST( test_vec_rbegin );
@@ -351,7 +459,18 @@ MU_TEST_SUITE( suite_vector )
     MU_RUN_TEST( test_vec_insert_pos_val );
     MU_RUN_TEST( test_vec_insert_pos_nsize_val );
     MU_RUN_TEST( test_vec_insert_pos_range );
-    MU_RUN_TEST( test_vec_erase_pos );
+    MU_RUN_TEST( test_vec_erase_first );
+    MU_RUN_TEST( test_vec_erase_range );
+    MU_RUN_TEST( test_vec_swap_empty );
+    MU_RUN_TEST( test_vec_swap );
+    MU_RUN_TEST( test_vec_clear );
+    MU_RUN_TEST( test_vec_allocator );
+    MU_RUN_TEST( test_vec_lesser_than );
+    MU_RUN_TEST( test_vec_less_than_or_equal );
+    MU_RUN_TEST( test_vec_greater_than );
+    MU_RUN_TEST( test_vec_greater_than_or_equal );
+    MU_RUN_TEST( test_vec_equal );
+    MU_RUN_TEST( test_vec_not_equal );
 }
 
 int main()
