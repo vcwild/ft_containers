c46dff7edd455adaf213b21a4ec05cabcfa20beb
Victor Wildner <vwildner@redhat.com>
2022-11-09 01:40:49 -0300

    fix: improve type traits implementation

--- includes/type_traits.hpp
+++ includes/type_traits.hpp
@@ -3,198 +3,209 @@
 
 #include <cstddef>
 
+struct true_type
+{
+    static const bool value = true;
+};
+
+struct false_type
+{
+    static const bool value;
+};
+
 namespace ft {
 
 template < typename T >
 struct is_integral
 {
-    static const bool type = false;
+    typedef false_type type;
 };
 
 template <>
 struct is_integral<bool>
 {
-    static const bool type = true;
+
+    typedef true_type type;
 };
 
 template <>
 struct is_integral<char>
 {
-    static const bool type = true;
+    typedef true_type type;
 };
 
 template <>
 struct is_integral<wchar_t>
 {
-    static const bool type = true;
+    typedef true_type type;
 };
 
 template <>
 struct is_integral<signed char>
 {
-    static const bool type = true;
+    typedef true_type type;
 };
 
 template <>
 struct is_integral<short int>
 {
-    static const bool type = true;
+    typedef true_type type;
 };
 
 template <>
 struct is_integral<int>
 {
-    static const bool type = true;
+    typedef true_type type;
 };
 
 template <>
 struct is_integral<long int>
 {
-    static const bool type = true;
+    typedef true_type type;
 };
 
 template <>
 struct is_integral<long long int>
 {
-    static const bool type = true;
+    typedef true_type type;
 };
 
 template <>
 struct is_integral<unsigned char>
 {
-    static const bool type = true;
+    typedef true_type type;
 };
 
 template <>
 struct is_integral<unsigned short int>
 {
-    static const bool type = true;
+    typedef true_type type;
 };
 
 template <>
 struct is_integral<unsigned int>
 {
-    static const bool type = true;
+    typedef true_type type;
 };
 
 template <>
 struct is_integral<unsigned long int>
 {
-    static const bool type = true;
+    typedef true_type type;
 };
 
 template <>
 struct is_integral<unsigned long long int>
 {
-    static const bool type = true;
+    typedef true_type type;
 };
 
 template < typename T >
 struct is_integral<const T>
 {
-    static const bool type = is_integral<T>::type;
+    typedef typename is_integral<T>::type type;
 };
 
 template < typename T >
 struct is_integral<volatile T>
 {
-    static const bool type = is_integral<T>::type;
+    typedef typename is_integral<T>::type type;
 };
 
 template < typename T >
 struct is_integral<const volatile T>
 {
-    static const bool type = is_integral<T>::type;
+    typedef typename is_integral<T>::type type;
 };
 
 template < typename T >
 struct is_floating_point
 {
-    static const bool type = false;
+    typedef true_type type;
 };
 
 template <>
 struct is_floating_point<float>
 {
-    static const bool type = true;
+    typedef true_type type;
 };
 
 template <>
 struct is_floating_point<double>
 {
-    static const bool type = true;
+    typedef true_type type;
 };
 
 template <>
 struct is_floating_point<long double>
 {
-    static const bool type = true;
+    typedef true_type type;
 };
 
 template < typename T >
 struct is_pointer
 {
-    static const bool type = false;
+    typedef true_type type;
 };
 
 template < typename T >
 struct is_pointer<T *>
 {
-    static const bool type = true;
+    typedef true_type type;
 };
 
 template < typename T >
 struct is_reference
 {
-    static const bool type = false;
+    typedef true_type type;
 };
 
 template < typename T >
 struct is_reference<T &>
 {
-    static const bool type = true;
+    typedef true_type type;
 };
 
 template < typename T >
 struct is_const
 {
-    static const bool type = false;
+    typedef true_type type;
 };
 
 template < typename T >
 struct is_const<const T>
 {
-    static const bool type = true;
+    typedef true_type type;
 };
 
 template < typename T >
 struct is_volatile
 {
-    static const bool type = false;
+    typedef true_type type;
 };
 
 template < typename T >
 struct is_volatile<volatile T>
 {
-    static const bool type = true;
+    typedef true_type type;
 };
 
 template < typename T >
 struct is_class
 {
-    static const bool type = false;
+    typedef true_type type;
 };
 
 template < typename T >
 struct is_union
 {
-    static const bool type = false;
+    typedef true_type type;
 };
 
 template < typename T >
 struct is_array
 {
-    static const bool type = false;
+    typedef true_type type;
 };
 
 template < bool B, typename T = void >
